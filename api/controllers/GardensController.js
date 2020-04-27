f0939a306c12d94e8f2d426ce108af38a8011659
Sebastián González <sgonzalez@debianchile.cl>
2020-04-26 22:33:22 -0400

    fix linter

--- api/controllers/GardensController.js
+++ api/controllers/GardensController.js
@@ -10,49 +10,49 @@ module.exports = {
     const polygon = req.param('polygon');
 
     if (typeof polygon === 'undefined') {
-      Gardens.find().exec(function (error, result) {
+      Gardens.find().exec((error, result) => {
         return res.json(result);
       });
     }
 
     try {
-      var coordinates = JSON.parse(polygon);
+      const coordinates = JSON.parse(polygon);
+
+      if (!Array.isArray(coordinates)) {
+        res.status(409);
+        return res.json({error: 'polygon: an array was expected'});
+      }
+
+      if (coordinates.length < 4) {
+        res.status(409);
+        return res.json({error: 'polygon: a minimum of 4 points was expected'});
+      }
+
+      var query = {
+        location: {
+          $geoWithin: {
+            $geometry: {
+              type: 'Polygon',
+              coordinates: [ coordinates ]
+            }
+          }
+        }
+      };
+
+      const db = Gardens.getDatastore().manager;
+      const gardenCollection = db.collection(Gardens.tableName);
+
+      gardenCollection.find(query).toArray((mongoErr, docs) => {
+        if (mongoErr) {
+          return res.json(mongoErr);
+        }
+
+        return res.json(docs);
+      });
     } catch(e) {
       res.status(409);
       return res.json({error: 'polygon: an array was expected'});
     }
-
-    if (!Array.isArray(coordinates)) {
-      res.status(409);
-      return res.json({error: 'polygon: an array was expected'});
-    }
-
-    if (coordinates.length < 4) {
-      res.status(409);
-      return res.json({error: 'polygon: a minimum of 4 points was expected'});
-    }
-
-    var query = {
-      location: {
-        $geoWithin: {
-          $geometry: {
-            type: 'Polygon',
-            coordinates: [ coordinates ]
-          }
-        }
-      }
-    };
-
-    const db = Gardens.getDatastore().manager;
-    const gardenCollection = db.collection(Gardens.tableName);
-
-    gardenCollection.find(query).toArray(function(mongoErr, docs) {
-      if (mongoErr) {
-        return res.json(mongoErr);
-      }
-
-      return res.json(docs);
-    });
   }
 };
 
