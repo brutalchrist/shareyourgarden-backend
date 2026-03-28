d392a879f1da42a3dc197c8cb1f074b760346c01
Sebastián González <brutalchrist@gmail.com>
2026-03-28 01:11:20 -0300

    chore: use env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GardensModule } from './gardens/gardens.module';
+import { env } from 'process';
 
 @Module({
   imports: [
     GardensModule,
     MongooseModule.forRoot(
-      'mongodb+srv://brutalchrist:wpgC5pc43qiyPrbC@cluster0.p5w4m.mongodb.net/shareyourgarden',
+      env.MONGODB_URI || 'mongodb://localhost:27017/shareyourgarden',
     ),
   ],
   controllers: [AppController],
